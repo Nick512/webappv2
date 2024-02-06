2c6268ce3c41d50881455c2830bebbb6bbe82d27
Nicholas <nkeren24@gmail.com>
2024-02-06 14:21:29 -0500

    added comments

--- server/api/deleteteam.ts
+++ server/api/deleteteam.ts
@@ -2,16 +2,19 @@ import { serverSupabaseServiceRole } from '#supabase/server'
 
 
 export default defineEventHandler(async (event) => {
+    //Get db connection
+    const supabase = await serverSupabaseServiceRole(event)
 
-  const supabase = await serverSupabaseServiceRole(event)
+    //Get team object from request body
+    const { team } = await readBody(event)
 
-  const { team } = await readBody(event)
+    //Get league
+    const { data } = await supabase.from('leagues').select('numOfTeams').eq('name', team.leagueName)
 
-  const { data } = await supabase.from('leagues').select('numOfTeams').eq('name', team.leagueName)
-
-
+    //Update number of teams in league
     let updatedNumber = data[0].numOfTeams - 1
 
+    //Delete team from db
     const { data:error2 } = await supabase.from('leagues').update({numOfTeams: updatedNumber}).eq('name', team.leagueName)
 
  
